0247daecefc3b1586976220f429aae304e32a0eb
Andrei Belousov <Andrei_Belousov@epam.com>
2020-06-08 19:27:35 +0300

    fix rp logger

--- jdi-dark-demo-rp-bdd/src/test/java/com/epam/jdi/tests/CucumberTestsRunner.java
+++ jdi-dark-demo-rp-bdd/src/test/java/com/epam/jdi/tests/CucumberTestsRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        plugin = {"pretty", "com.epam.reportportal.cucumber.StepReporter"},
+        plugin = {"pretty", "com.epam.reportportal.cucumber.ScenarioReporter"},
         features = "src/test/resources/features/",
         glue = {"com/epam/jdi/tests/steps", "com/epam/jdi/http/stepdefs/en"})
 public class CucumberTestsRunner extends AbstractTestNGCucumberTests {

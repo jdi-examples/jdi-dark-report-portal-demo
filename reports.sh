41cc9aebee8e994e27e883acd21353d4ceb447e2
Andrei Belousov <Andrei_Belousov@epam.com>
2020-06-11 17:44:54 +0300

    fix codacy

--- reports.sh
+++ reports.sh
@@ -40,8 +40,8 @@ function collectRelevantComments(){
 function sendComment() {
     body="$1"
     url="https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
-    echo Body: ${body}
-    echo URL: ${url}
+    echo Body: "${body}"
+    echo URL: "${url}"
     curl -H "Authorization: token ${GIT_COMMENT_USER}" \
          -X POST "${url}" \
          -d "{
@@ -89,10 +89,10 @@ function grubAllureResults() {
     if [[ "x${TRAVIS_BUILD_STAGE_NAME}" == "xtest" ]] ; then #don't remove x, it's useful
         for result in $(find jdi-dark*/target/allure-results -maxdepth 1 -type d)
         do
-            echo RESULT: ${result}
+            echo RESULT: "${result}"
             archiveFile="$(archive "${result}")"
             echo ARCHIVE: "${archiveFile}"
-            ls -lah *.tar.gz
+            ls -lah ./"${archiveFile}"
             uploadedTo="$(uploadFile "${archiveFile}")"
             echo UPLOAD TO KEY: "${uploadedTo}"
             sendComment "$(aboutTransfer "${uploadedTo}")"
@@ -120,7 +120,7 @@ function deployAllureResults() {
     echo "LOG1"
     url="$(deployToNetlify "allure-report")"
     echo "LOG2"
-    sendComment "$(aboutNetlify ${url})"
+    sendComment "$(aboutNetlify "${url}")"
 }
 
 function downloadAllureResults() {
@@ -130,8 +130,6 @@ function downloadAllureResults() {
     do
         urlExistence=true
         echo "Found: ${urlKey}"
-        # TODO: $4 for file.io, #5 for transfer.sh, add an IF
-        #fileName="$(echo "${url}.tar.gz"| awk -F/ '{print $5}')"
         fileName="${urlKey}.tar.gz"
         curl https://file.io/"${urlKey}" --output "${fileName}"
     done
@@ -144,7 +142,7 @@ function downloadAllureResults() {
 }
 
 function extractAllureResults() {
-    for archiveFile in $(ls -1 *.tar.gz)
+    for archiveFile in $(ls -1 ./*.tar.gz)
     do
         extractArchive "${archiveFile}"
     done
@@ -168,8 +166,8 @@ function generateAllureReports() {
         echo "Failed inside generateAllureReports()"
         exitWithError
     fi
-    echo ${reportDirList}
-    allure generate --clean ${reportDirList}
+    echo "${reportDirList}"
+    allure generate --clean "${reportDirList}"
 }
 
 function deployToNetlify() {
